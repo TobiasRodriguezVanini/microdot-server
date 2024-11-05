06a7251a88e011ed18c3aeb948a5fcc776385753
TobiasRodriguezVanini <81983303+TobiasRodriguezVanini@users.noreply.github.com>
2024-11-05 10:16:06 -0300

    feat: subir Actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,20 @@
-# Aplicacion del servidor
+from microdot import Microdot, send_file 
+# Importo Microdot y la función send_file desde la libreria microdot
+
+app = Microdot() 
+# Se instancia Microdot
+
+# Definimos la ruta raíz
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+
+@app.route('/<dir>/<file>') 
+# Definimos la ruta 
+async def index(request, dir, file):
+    return send_file("/" + dir + "/" + file) 
+ # Nombre del archivo a enviar
+
+# Prendemos el servidor en el puerto 80
+app.run(port=80)
